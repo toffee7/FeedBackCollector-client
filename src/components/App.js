0806b80c2fd0aec358204ae1319d2c6b308f907c
Tauseef <syed.tauseef.tanweer@citi.com>
2017-09-13 17:40:07 +0530

    Header created

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './Header';
 import 'bootstrap/dist/css/bootstrap.css';
+
 
 const App = () => {
     return (
-        <div className="btn btn-success">
-            Project Toffee
+        <div>
+            <Header />
         </div>
     );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
-const Header =(props) => {
+const Header = (props) => {
     return (
-        <nav className="">
-
-        
+        <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+            <a className="navbar-brand" href="/">Project Toffee</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarText">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">Features</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">Pricing</a>
+                    </li>
+                </ul>
+                <button className="btn btn-sm align-middle btn-outline-secondary" type="button">Sign In</button>
+            </div>
         </nav>
     );
 }
